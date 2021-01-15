fb4417c8bd781dd9d1de8b8f0794b32cf7b62b59
xmjt1997 <947705624@qq.com>
2021-01-15 16:25:24 +0800

    typescript

---
+++ TypeScript/part1/index.ts
@@ -0,0 +1,12 @@
+// number、string、boolean、字面量、any、unknown、void、never、object、array、tuple、enum
+
+let a:number=100
+let b:string='100'
+let c:boolean=true
+let d:unknown=1231
+let e:any='1231'
+let f = function():void{}
+let g:object
+let h:[1]
+let i:[string,number]
+i=[123,2313]
