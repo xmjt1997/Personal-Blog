2dd998c6c78c5c4c2ae3fcfcb502695ef8da7c4e
xmjt1997 <947705624@qq.com>
2021-01-31 21:59:02 +0800

    vue3 学习

---
+++ vue3_study/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  setup(){
+    console.log('setup');
+    const cout = ref(1)
+    console.log(cout.value);
+    
+    
+  }
+});
+</script>
