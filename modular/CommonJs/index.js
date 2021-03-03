09398b829a9d65a41528460d6ff974c0d86af258
xmjt1997 <947705624@qq.com>
2021-03-03 16:21:00 +0800

    css布局

--- modular/CommonJs/index.js
+++ modular/CommonJs/index.js
@@ -1,14 +1,5 @@
+const { Script } = require("vm");
 
-(function(){
-    var obj = {
-        name:'lcz',
-        age:24
-    }
-    
-    function add(obj){
-        console.log(obj.name,obj.age);
-    }
-    window.obj=obj
-    window.add = add
-})(window)
+
+console.log(a);
 
